98f0cfbc7ece6a156a25a333023c930f5ba7ee67
Edith <ebalbouze@medine.dev>
2022-03-04 16:15:05 -0400

    actualizacion1

--- newexercice.js
+++ newexercice.js
@@ -1,3 +1,5 @@
+//  ARRAY
+
 var frutas = ["Manzana", "Fresa", "Cereza", "Naranja"];
 
 function printFirstElement(arreglo){
@@ -5,3 +7,25 @@ function printFirstElement(arreglo){
 }
 
 printFirstElement(frutas);
+
+function printAllElement(arreglo){
+    for(var i = 0; i <= arreglo.length - 1; i++){
+        console.log(arreglo[i]);
+    }
+}
+printAllElement(frutas);
+
+// OBJETOS
+var user = {
+    firsName: "Paullete",
+    lastName: "Malcoum",
+    age:21,
+}
+
+function printAllElementObjets(objeto){
+    for(var key in user){
+        console.log(user[key]);
+    }
+}
+
+printAllElementObjets(user);
