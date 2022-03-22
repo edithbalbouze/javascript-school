0e617279e26b758eaaddc318b77c65de84cfdaef
Edith <ebalbouze@medine.dev>
2022-03-22 10:44:12 -0400

    Nuevas practicas

---
+++ salarios.js
@@ -0,0 +1,101 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Miguel",
+    salary: 1200,
+});
+colombia.push({
+    name: "Anna",
+    salary: 1000,
+});
+colombia.push({
+    name: "Maria",
+    salary: 1500,
+});
+colombia.push({
+    name: "Samuel",
+    salary: 1000,
+});
+colombia.push({
+    name: "Martina",
+    salary: 1100,
+});
+colombia.push({
+    name: "Misa",
+    salary: 1200,
+});
+colombia.push({
+    name: "Jose",
+    salary: 500,
+});
+colombia.push({
+    name: "Julia",
+    salary: 1100,
+});
+colombia.push({
+    name: "JuanitaMartina",
+    salary: 1150,
+});
+colombia.push({
+    name: "Juan",
+    salary: 1100,
+});
+colombia.push({
+    name: "Jinna",
+    salary: 1300,
+});
+colombia.push({
+    name: "Carla",
+    salary: 2100,
+});
+colombia.push({
+    name: "JMila",
+    salary: 1000,
+});
+colombia.push({
+    name: "JRobeto",
+    salary: 2100,
+});
+colombia.push({
+    name: "Justa",
+    salary: 900,
+});
+colombia.push({
+    name: "Jesus",
+    salary: 800,
+});
+colombia.push({
+    name: "Ingrid",
+    salary: 1400,
+});
+colombia.push({
+    name: "Ramona",
+    salary: 1100,
+});
+colombia.push({
+    name: "Roberta",
+    salary: 700,
+});
+colombia.push({
+    name: "Alberto",
+    salary: 600,
+});
+colombia.push({
+    name: "Gibmyx",
+    salary: 600,
+});
+colombia.push({
+    name: "Daniel",
+    salary: 700,
+});
+colombia.push({
+    name: "Jimena",
+    salary: 1100,
+});
+colombia.push({
+    name: "Gabriel",
+    salary: 800,
+});
